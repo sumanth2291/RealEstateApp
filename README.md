fc308415149adb5961b1e820e75a0eedd7ed5935
Sumanth <Sumanth@Adarshs-MacBook-Air.local>
2018-02-08 21:23:11 +0100

    Project Setup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # RealEstateApp
+
+```
+npm install 
+npm run start
+```
+*Reactstrap*
+https://reactstrap.github.io/components/alerts/
+
+*GSAP*
+https://greensock.com/docs/
+
+*Styled Components*
+https://www.styled-components.com/docs/basics
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles/app.scss";
+import React from "react";
+import ReactDOM, { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import App from "./components/app";
+
+const renderApp = Component =>
+	render(
+		<Component />,
+		document.getElementById("root")
+	);
+
+renderApp(App);
+
+if (module.hot) module.hot.accept("./components/app", () => renderApp(App));
